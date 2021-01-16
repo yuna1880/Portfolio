121b5be2fcf9049af1bef1bafce3847e68cb4450
yuna1880 <72509037+yuna1880@users.noreply.github.com>
2021-01-16 23:47:30 +0900

    JS : home contact button

--- js/main.js
+++ js/main.js
@@ -25,7 +25,18 @@ navbarMenu.addEventListener('click', (event) => {
     if(link == null){
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior : "smooth"});
+    //console.log(event.target.dataset.link);
+    scrollIntoView(link);
 });
+
+//contact 버튼 선택시, Scrolling되어 화면 이동.
+const contactBtn = document.querySelector('.home_contact');
+contactBtn.addEventListener('click', (event) =>{
+    scrollIntoView('#contact');
+});
+
+//
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior : "smooth"} );
+}
