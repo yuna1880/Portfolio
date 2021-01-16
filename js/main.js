5fef2a6e9ea29ab9055c18c5f83505b379b66d7d
yuna1880 <72509037+yuna1880@users.noreply.github.com>
2021-01-16 22:49:04 +0900

    JS : navbar menu click

--- js/main.js
+++ js/main.js
@@ -6,12 +6,26 @@ const navbarHeight = navbar.getBoundingClientRect().height;
 
 //Navbar 투명하게 만들기.
 //애로우함수 -> 아무런 인자 받지 않고 실행
-document.addEventListener('scroll', ()=>{
-    console.log(window.scrollY); // 스크롤 값 표시
-    console.log('navbar height : ' + navbarHeight); //네비바 높이 표시
-    if(window.scrollY > navbarHeight){
+document.addEventListener('scroll', () => {
+    //console.log(window.scrollY); // 스크롤 값 표시
+    //console.log('navbar height : ' + navbarHeight); //네비바 높이 표시
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
-    }else{
+    } else {
         navbar.classList.remove('navbar--dark');
     }
+});
+
+//navbar의 메뉴 선택시, Scrolling되어 화면 이동.
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+
+    if(link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior : "smooth"});
 });
